a94dcc0ed21b9b97a828c84ec146fefcaabefe1c
grynosai <aamarles24@gmail.com>
2024-01-25 10:09:51 +0800

    tan den

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-md-3 mb-4">
                     <div class="card" style="width: 14rem; margin: 0 auto;">
                         <div class="card-body text-center">
-                            
+                        <p class="card-text">{{ $authorCount }}</p>
                             <h5 class="card-title mb-0">Authors Listed</h5>
                         </div>
                     </div>
@@ -16,7 +16,7 @@
                 <div class="col-md-3 mb-4">
                     <div class="card" style="width: 14rem; margin: 0 auto;">
                         <div class="card-body text-center">
-                            
+                        <p class="card-text">{{ $categoryCount }}</p>
                             <h5 class="card-title mb-0">Categories</h5>
                         </div>
                     </div>
@@ -24,7 +24,7 @@
                 <div class="col-md-3 mb-4">
                     <div class="card" style="width: 14rem; margin: 0 auto;">
                         <div class="card-body text-center">
-                            
+                        <p class="card-text">{{ $bookCount }}</p>
                             <h5 class="card-title mb-0">Books Listed</h5>
                         </div>
                     </div>
@@ -32,7 +32,7 @@
                 <div class="col-md-3 mb-4">
                     <div class="card" style="width: 14rem; margin: 0 auto;">
                         <div class="card-body text-center">
-                           
+                        <p class="card-text">{{ $profileCount }}</p>
                             <h5 class="card-title mb-0">Profiles</h5>
                         </div>
                     </div>
@@ -40,7 +40,7 @@
                 <div class="col-md-3">
                     <div class="card" style="width: 14rem; margin: 0 auto;">
                         <div class="card-body text-center">
-                            
+                        <p class="card-text">{{ $bookIssueCount }}</p>
                             <h5 class="card-title mb-0">Books Issued</h5>
                         </div>
                     </div>
